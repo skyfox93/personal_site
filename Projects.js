890bf7e79077f0444b8cf2f3d6211465ea1eccfa
Skylar Salerno <skylar.salerno@gmail.com>
2022-10-13 13:06:47 -0700

    clean up javascript

---
+++ Projects.js
@@ -0,0 +1,78 @@
+/* TODO: Reorganize Code To Match Best Practice*/ 
+
+
+let projects = [
+    //Title, description, url, VideoURL
+    {
+      description: "Share and edit your friend's photos",
+      title: 'Spinoff',
+      url: 'http://174.138.58.32/spinoff/',
+      videoURL: "https://player.vimeo.com/video/331844687?byline=0&portrait=0",
+      imageURL: './images/spinoff.png',
+      code: "https://github.com/skyfox93/spinoff-front",
+      tech: ['Ruby on Rails', 'React', 'Javascript']
+    },
+  
+    {
+      title: 'Plunk!',
+      description: 'Jam with other people in real-time',
+      url: 'https://skyfox93-plunk.glitch.me/',
+      videoURL: "https://player.vimeo.com/video/332441007?portrait=0",
+      imageURL: './images/plunk.png',
+      code: "https://github.com/skyfox93/plunk",
+      tech: ['NodeJS', "Socket.io", 'Javascript']
+    }
+  ]
+
+  class Project {
+
+    constructor(data) {
+        this.data = data
+    }
+
+    getHtml() {
+        return (
+            `
+            <div class='card'>
+                <div class='card-title'>
+                ${this.data.title}
+                </div>
+                <div class='card-image' style='background-image:url(${this.data.imageURL})'>
+                </div>
+                <div class='card-description'>
+                ${this.data.description}
+                </div>
+                <div class='built-with'> Built with: ${this.data.tech.map(
+                (t) => (`<span class="tech-keyword">${t}</span>`)
+            ).join('')}
+                </div>
+                <div class='card-menu'>
+                ${this.data.videoURL ?
+                `<div data-url="${this.data.videoURL}" class='menu-link video'> Video </div>`
+                : ''}
+                ${this.data.url ?
+                `<a href="${this.data.url}" class='menu-link'>Demo </a>`
+                : ''}
+                ${this.data.code ?
+                `<a href='${this.data.code}' class='menu-link'> Repo </a>` :
+                ''}
+                </div>
+            </div>
+        `
+    )   
+    }
+  }
+
+  let initalizedProjects = projects.map(data => new Project(data))
+  
+  let renderProjects = (projects, projectsContainer) => {
+      let newContent = (initalizedProjects).reduce((acc,project) =>(acc + project.getHtml()), '')
+      projectsContainer.innerHTML = newContent
+  }
+  
+  const projectsContainer = document.querySelector('#projects-container')
+  console.log(projectsContainer)
+  renderProjects(initalizedProjects,projectsContainer)
+  
+  
+  
--- main.js
+++ main.js
@@ -1,91 +1,10 @@
-
-let projects = [
-  //Title, description, url, VideoURL
-  // title must match object key
-  {
-    description: "Share and edit your friend's photos",
-    title: 'Spinoff',
-    url: 'http://174.138.58.32/spinoff/',
-    videoURL: "https://player.vimeo.com/video/331844687?byline=0&portrait=0",
-    imageURL: './images/spinoff.png',
-    code: "https://github.com/skyfox93/spinoff-front",
-    tech: ['Ruby on Rails', 'React', 'Javascript']
-  },
-
-  //  {
-  //    description:'Complete a list, collect a kitten' ,
-  //    title:'TaskKittens',
-  //    url: null,
-  //    videoURL:"https://player.vimeo.com/video/331912608?byline=0&portrait=0",
-  //    imageURL:'./images/taskittens2.png',
-  //    code:  "https://github.com/skyfox93/taskKittens-front",
-  //    tech: 'Ruby on Rails, Javascript'
-  //  },
-  {
-    title: 'Plunk!',
-    description: 'Jam with other people in real-time',
-    url: 'https://skyfox93-plunk.glitch.me/',
-    videoURL: "https://player.vimeo.com/video/332441007?portrait=0",
-    imageURL: './images/plunk.png',
-    code: "https://github.com/skyfox93/plunk",
-    tech: ['NodeJS', "Socket.io", 'Javascript']
-  }
-]
+import './Projects.js'
 
 const projectsContainer = document.querySelector('#projects-container')
-const videoContainer = document.querySelector('#video-container');
-const videoEl = document.querySelector('#video');
-let vidPlay = false;
-renderProjects(projects, projectsContainer)
+projectsContainer.addEventListener('click', playVid)
 
-
-/// helpers///////
-
-function projectHTML(project) {
-  console.log(project.videoURL);
-  return `
-  <div class='card'>
-    <div class='card-title'>
-      ${project.title}
-    </div>
-    <div class='card-image' style='background-image:url(${project.imageURL})'>
-    </div>
-    <div class='card-description'>
-      ${project.description}
-    </div>
-    <div class='built-with'> Built with: ${project.tech.map(
-    (t) => (`<span class="tech-keyword">${t}</span>`)
-  ).join('')}
-    </div>
-    <div class='card-menu'>
-      ${project.videoURL ?
-      `<div data-url="${project.videoURL}" class='menu-link video'> Video </div>`
-      : ''}
-      ${project.url ?
-      `<a href="${project.url}" class='menu-link'>Demo </a>`
-      : ''}
-      ${project.code ?
-      `<a href='${project.code}' class='menu-link'> Repo </a>` :
-      ''}
-    </div>
-  </div>
-  `
-}
-
-function projectReducer(htmlString, project) {
-  console.log(projectHTML(project))
-  return htmlString + projectHTML(project)
-
-}
-function renderProjects(projects, container) {
-  let htmlString = projects.reduce(projectReducer, '')
-  projectsContainer.innerHTML = (htmlString)
-  projectsContainer.addEventListener('click', playVid)
-}
-
-
+//VIDEO PLAYING LOGIC ///
 function playVid(e) {
-  console.log(e.target)
   if (e.target.className === 'menu-link video') {
     videoEl.src = e.target.dataset.url
     videoContainer.style.display = 'block'
@@ -94,50 +13,20 @@ function playVid(e) {
     videoEl.scrollIntoView();
   }
 }
-
-let banner = document.querySelector('.title-banner');
-let spacer = document.querySelector('#spacer')
-console.log(banner);
-let title = document.querySelector('.title');
-let main = document.querySelector('.main')
-let desc1 = document.querySelectorAll('.description')[0]
-let desc2 = document.querySelectorAll('.description')[1]
-let nav = document.querySelector('.nav')
-let hamburger = document.querySelector('#hamburger')
-hamburger.addEventListener('click', toggleResponsive);
-let closeNav = document.querySelector('#close-nav')
-closeNav.addEventListener('click', toggleResponsive)
-
-let lastScroll = 0;
-
-function toggleResponsive(e) {
-  e.preventDefault()
-  console.log('responsive!')
-  nav.className = (
-    nav.className === 'nav-responsive' ? 'nav-collapsed' : 'nav-responsive'
-  )
-}
+const videoContainer = document.querySelector('#video-container');
+const videoEl = document.querySelector('#video');
+let vidPlay = false;
 
 document.querySelector('#video-close').addEventListener(
   'click', (e) => { videoContainer.style.display = "none" }
 )
 
-
+//* Opacity Scroll Effect */
 const bannerContent = document.querySelector('.title-banner-main')
+const handleOpacityEffect = (e) => {
 
-
-const handleTextResize = (e) => {
-
-  let fontsize = 32 - document.body.scrollTop / 10
-  console.log(fontsize)
   let opacity = 1 - document.body.scrollTop / 200
   bannerContent.style.opacity = opacity
-  if (fontsize < 12) {
-    fontsize = 12
-  }
-  if (fontsize > 32) {
-    fontsize = 32
-  }
-  title.style.fontSize = fontsize + 'px'
 }
-document.addEventListener('scroll', handleTextResize)
+
+document.addEventListener('scroll', handleOpacityEffect)
