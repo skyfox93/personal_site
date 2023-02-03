7aa368f905b860fbc96153c68fc2e665a5cb1396
Skylar Salerno <skylar.salerno@gmail.com>
2023-02-03 16:08:34 -0500

    update to include blog posts

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import './Projects.js'
+import Glide from './node_modules/@glidejs/glide/dist/glide.modular.esm.js'
 
 particlesJS.load("projects", "./particles.json", function () {
   console.log("callback - particles.js config loaded");
 });
+
+
+
 
 const projectsContainer = document.querySelector('#projects-container')
 projectsContainer.addEventListener('click', playVid)
@@ -24,3 +28,44 @@ let vidPlay = false;
 document.querySelector('#video-close').addEventListener(
   'click', (e) => { videoContainer.style.display = "none" }
 )
+let parser = new RSSParser();
+parser.parseURL("https://corsanywhere.herokuapp.com/medium.com/feed/skylar-salernos-tech-blog", function(err, feed) {
+  if (err) throw err;
+  console.log(feed.items);
+  const blogsContainer = document.querySelector('#blog-container')
+
+  feed.items.forEach((item) => {
+  let div = document.createElement('div')
+  div.className='swiper-slide blog-preview'
+  div.innerHTML = `
+  <a href="${item.link}"><h3> ${item.title}</h3> 
+      <div class="description">
+          ${item["content:encodedSnippet"].slice(0, 150)}...
+      </div>
+      </a> 
+    `
+  blogsContainer.appendChild(div)
+  })
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+})
+
