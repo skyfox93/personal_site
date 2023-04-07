88fbade4494c7ec3f28b0023f94e39d7bd6777cb
Skylar Salerno <skylar.salerno@gmail.com>
2023-04-07 19:14:59 -0400

    bugfix video play

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ particlesJS.load("projects", "./particles.json", function () {
   console.log("callback - particles.js config loaded");
 });
 
-
-
-
 const projectsContainer = document.querySelector('#projects-container')
 projectsContainer.addEventListener('click', playVid)
 
 //VIDEO PLAYING LOGIC ///
+
+const videoContainer = document.querySelector('#video-container');
 function playVid(e) {
+  const videoEl = document.querySelector('#video');
   if (e.target.className === 'menu-link video') {
     videoEl.src = e.target.dataset.url
     videoContainer.style.display = 'block'
@@ -21,9 +21,6 @@ function playVid(e) {
     videoEl.scrollIntoView();
   }
 }
-const videoContainer = document.querySelector('#video-container');
-const videoEl = document.querySelector('#video');
-let vidPlay = false;
 
 document.querySelector('#video-close').addEventListener(
   'click', (e) => { videoContainer.style.display = "none" }
